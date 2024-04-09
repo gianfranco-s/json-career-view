bc9723a12860d46064d82d7592405c659eebe2c2
gsalomone <gianfranco.s@gmail.com>
2024-04-09 19:26:38 -0300

    Fix typescript in WorkExperienceItem

--- my-json-resume/src/components/WorkExperienceItem.tsx
+++ my-json-resume/src/components/WorkExperienceItem.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 
-function WorkExperienceItem({ workExperienceItem }) {
-    return (
-        <div>
-            <p>{workExperienceItem.position}</p>
-            <p>{workExperienceItem.company}</p>
-            <p>{workExperienceItem.yearStart}</p>
-            <p>{workExperienceItem.yearEnd}</p>
-            <p>{workExperienceItem.isRemote}</p>
-            <p>{workExperienceItem.location}</p>
-            <p>{workExperienceItem.decription}</p>
-            <br />
-        </div>
-    );
+interface WorkExperienceItemProps {
+  workExperienceItem: {
+    position: string;
+    company: string;
+    yearStart: string;
+    yearEnd: string;
+    isRemote: boolean;
+    location: string;
+    decription: string;
+  };
+}
+
+function WorkExperienceItem({ workExperienceItem }: WorkExperienceItemProps) {
+  return (
+    <div>
+      <p>{workExperienceItem.position}</p>
+      <p>{workExperienceItem.company}</p>
+      <p>{workExperienceItem.yearStart}</p>
+      <p>{workExperienceItem.yearEnd}</p>
+      <p>{workExperienceItem.isRemote ? 'Remote' : 'On-site'}</p>
+      <p>{workExperienceItem.location}</p>
+      <p>{workExperienceItem.decription}</p>
+      <br />
+    </div>
+  );
 }
 
 export default WorkExperienceItem;
