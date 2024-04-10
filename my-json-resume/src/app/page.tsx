4119dba83481eca78ef7decf848d1549cb6d2de9
gsalomone <gianfranco.s@gmail.com>
2024-04-10 01:00:02 -0300

    Add  to debug css

--- my-json-resume/src/app/page.tsx
+++ my-json-resume/src/app/page.tsx
@@ -9,9 +9,9 @@ import AboutCard from '../components/AboutCard'
 
 const Home = () => {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 border">
 
-      <div className="flex justify-center">
+      <div className="flex justify-center border">
         <div className="w-full md:w-1/2"> {/* Center title and contact info */}
           <Title titleData={resumeData.titleData} />
           <div className="mb-5 mt-5">
@@ -21,23 +21,25 @@ const Home = () => {
       </div>
 
 
-      <div className="container mx-auto px-4 py-8">
+      <div className="container mx-auto px-4 py-8 border">
         <div className="mb-10">
           <WorkExperienceCard workExperience={resumeData.workExperience} />
         </div>
       </div>
 
-      <div className="mb-10">
-        <SkillsCard skills={resumeData.skills} />
-      </div>
-      <div className="mb-10">
-        <EducationCard education={resumeData.education} />
-      </div>
-      <div className="mb-10">
-        <SpokenLanguagesCard spokenLanguages={resumeData.spokenLanguages} />
-      </div>
+      <div className="container mx-auto px-4 py-8 border">
+        <div className="mb-10">
+          <SkillsCard skills={resumeData.skills} />
+        </div>
+        <div className="mb-10">
+          <EducationCard education={resumeData.education} />
+        </div>
+        <div className="mb-10">
+          <SpokenLanguagesCard spokenLanguages={resumeData.spokenLanguages} />
+        </div>
 
-      <AboutCard about={resumeData.about} />
+        <AboutCard about={resumeData.about} />
+      </div>
     </div>
   );
 };
