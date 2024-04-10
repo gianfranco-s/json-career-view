1d4f6c5ec00a071abbd9a4ae8a5495540696299f
gsalomone <gianfranco.s@gmail.com>
2024-04-09 21:58:20 -0300

    Fix typing error for WorkExperienceItem

--- my-json-resume/src/components/WorkExperienceCard.tsx
+++ my-json-resume/src/components/WorkExperienceCard.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import WorkExperienceItem from './WorkExperienceItem';
+import { WorkExperienceItemDataProps } from './WorkExperienceItem';
 
-function WorkExperienceCard({ workExperience }) {
+interface WorkExperienceCardProps {
+    workExperience: {
+        sectionTitle: string;
+        sectionList: WorkExperienceItemDataProps[];
+    };
+}
+
+function WorkExperienceCard({ workExperience }: WorkExperienceCardProps) {
     return (
         <div>
             <h2 className="text-2xl font-bold mb-4">{workExperience.sectionTitle}</h2>
-            <WorkExperienceItem workExperienceItem={workExperience.sectionList[0]} />
-            <WorkExperienceItem workExperienceItem={workExperience.sectionList[1]} />
-            <WorkExperienceItem workExperienceItem={workExperience.sectionList[2]} />
-            <WorkExperienceItem workExperienceItem={workExperience.sectionList[3]} />
-            <WorkExperienceItem workExperienceItem={workExperience.sectionList[4]} />
-            <WorkExperienceItem workExperienceItem={workExperience.sectionList[5]} />
+            <WorkExperienceItem workExperienceItemData={workExperience.sectionList[0]}/>
+            <WorkExperienceItem workExperienceItemData={workExperience.sectionList[1]} />
+            <WorkExperienceItem workExperienceItemData={workExperience.sectionList[2]} />
+            <WorkExperienceItem workExperienceItemData={workExperience.sectionList[3]} />
+            <WorkExperienceItem workExperienceItemData={workExperience.sectionList[4]} />
+            <WorkExperienceItem workExperienceItemData={workExperience.sectionList[5]} />
         </div>
     );
 }
--- my-json-resume/src/components/WorkExperienceItem.tsx
+++ my-json-resume/src/components/WorkExperienceItem.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
 
-interface WorkExperienceItemProps {
-  workExperienceItem: {
-    position: string;
-    company: string;
-    yearStart: string;
-    yearEnd: string;
-    isRemote: boolean;
-    location: string;
-    decription: string;
-  };
+export interface WorkExperienceItemDataProps {
+    workExperienceItemData: {
+        id: string;
+        position: string;
+        company: string;
+        yearStart: string;
+        yearEnd: string;
+        isRemote: boolean;
+        location: string;
+        decription: string;
+    };
 }
 
-function WorkExperienceItem({ workExperienceItem }: WorkExperienceItemProps) {
-  return (
-    <div>
-      <p>{workExperienceItem.position}</p>
-      <p>{workExperienceItem.company}</p>
-      <p>{workExperienceItem.yearStart}</p>
-      <p>{workExperienceItem.yearEnd}</p>
-      <p>{workExperienceItem.isRemote ? 'Remote' : 'On-site'}</p>
-      <p>{workExperienceItem.location}</p>
-      <p>{workExperienceItem.decription}</p>
-      <br />
-    </div>
-  );
+function WorkExperienceItem({ workExperienceItemData }: WorkExperienceItemDataProps) {
+    return (
+        <div>
+            <p>{workExperienceItemData.position}</p>
+            <p>{workExperienceItemData.company}</p>
+            <p>{workExperienceItemData.yearStart}</p>
+            <p>{workExperienceItemData.yearEnd}</p>
+            <p>{workExperienceItemData.isRemote ? 'Remote' : 'On-site'}</p>
+            <p>{workExperienceItemData.location}</p>
+            <p>{workExperienceItemData.decription}</p>
+            <br />
+        </div>
+    );
 }
 
 export default WorkExperienceItem;
