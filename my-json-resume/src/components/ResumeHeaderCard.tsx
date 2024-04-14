40fb1c4c8863921ed479fae8dcbee8149afa3024
gian <gianfranco.s@gmail.com>
2024-04-14 15:16:25 -0300

    Create Resume component

--- my-json-resume/src/components/ResumeHeaderCard.tsx
+++ my-json-resume/src/components/ResumeHeaderCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Title from '../components/ResumeTitle'
+import Title from '@/components/ResumeTitle'
 import ContactInfoCard from './ContactInfoCard'
 import { ContactInfo } from './typesCustom';
 
