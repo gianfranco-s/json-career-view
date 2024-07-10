77cfc689a412b70b5ae74482eddee65b57308d7f
gsalomone <gianfranco.s@gmail.com>
2024-07-10 09:04:27 -0300

    Render education conditionally with url

--- my-json-resume/src/components/EducationItem.tsx
+++ my-json-resume/src/components/EducationItem.tsx
@@ -9,9 +9,20 @@ function EducationItem({ educationItemData }: EducationItemDataProps) {
     const startYear = new Date(educationItemData.startDate).getFullYear();
     const endYear = educationItemData.endDate ? new Date(educationItemData.endDate).getFullYear() : 'Present';
     const yearSpan = `${startYear} - ${endYear}`;
+    const educationItem = <h4 className="text-sm font-medium mb-1">{educationItemData.area}</h4>;
+    const urlConditional = !educationItemData.url
+                            ? educationItem
+                            : (
+                            <a href={educationItemData.url} 
+                               className="hover:underline" 
+                               target="_blank"
+                               rel="noopener noreferrer">
+                                {educationItem}
+                            </a>
+                            )
     return (
         <div className="mb-4">
-            <h4 className="text-sm font-medium mb-1">{educationItemData.area}</h4>
+            {urlConditional}
             <p className="text-sm mb-2">({yearSpan}) {educationItemData.institution}</p>
         </div>
     );
